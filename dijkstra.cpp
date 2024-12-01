dbce0ad4067c5c906f5275bc4caaf60119344e2a
Chayan Neogi <136154400+Chayanethic@users.noreply.github.com>
2024-12-01 19:57:29 +0530

    Dijkstra algorithm
    return the distance array that index value is source to that node min distance

---
+++ dijkstra.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define P pair<int, int>
+vector<int> dijkstra(vector<vector<P>> &adj, int n, int src)
+{
+  vector<int> result(n, INT_MAX);
+
+  priority_queue<P, vector<P>, greater<P>> pq; // min heap
+  pq.push({0, src});
+  result[src] = 0;
+
+  while (!pq.empty())
+  {
+    auto p = pq.top();
+    int wt = p.first;
+    int node = p.second;
+
+    for (auto &vec : adj[node])
+    {
+      int neighbour_node = vec.first;
+      int neighbour_wt = vec.second;
+
+      if (wt + neighbour_wt < result[neighbour_node])
+      {
+        result[neighbour_node] = wt + neighbour_node;
+        pq.push({wt + neighbour_node, neighbour_node});
+      }
+    }
+  }
+  return result;
+}
